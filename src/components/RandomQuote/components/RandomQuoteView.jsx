0f445acb16725d2657a0e805a8a0b40f8aff5bb1
Pakata Goh <pakatagohlh@gmail.com>
2019-03-06 16:01:30 +0800

    Install Lodash. Add RandomQuoteView Component and test

--- src/components/RandomQuote/components/RandomQuoteView.jsx
+++ src/components/RandomQuote/components/RandomQuoteView.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const RandomQuoteView = props => {
   const { randomQuote } = props;
-  return <div data-testid="random-quote-view">{randomQuote && randomQuote}</div>;
+  return (
+    <div data-testid="random-quote-view">
+      {randomQuote ? (
+        <>
+          <p>{randomQuote.name}</p>
+          <p>{randomQuote.text}</p>
+          <p>{randomQuote.episode}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default RandomQuoteView;
