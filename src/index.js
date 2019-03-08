4aa0e916a3cbc4f0e582f0b39eb71b30b8fdd246
Pakata Goh <pakatagohlh@gmail.com>
2019-03-08 16:38:10 +0800

    Add global styles for body and text

--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    min-height: 100%;
+    background-color: black;
+    color: white;
+  }
+`;
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <>
+      <GlobalStyles />
+      <App />
+    </>
   </BrowserRouter>,
   document.getElementById('root')
 );
