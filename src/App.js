afffba3ac9d5ab12bcbf139a83447fc99aea924c
Pakata Goh <pakatagohlh@gmail.com>
2019-03-24 16:19:47 +0800

    test proess.env.NODE_ENV

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Routing from './pages/Routing';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
+console.log('process.env.NODE_EV', process.env.NODE_ENV);
 class App extends Component {
   render() {
     return (
