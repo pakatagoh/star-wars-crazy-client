356e4c86ccb1f3a537c743988fa9ef5705ed41ba
Pakata Goh <pakatagohlh@gmail.com>
2019-10-05 18:36:51 +0800

    move async function into useEffect

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -8,16 +8,15 @@ import { getQuote } from '../../services/randomQuotes/randomQuotesService';
 const RandomQuote = () => {
   const [quote, setQuote] = useState('');
 
-  const handleQuote = async () => {
-    try {
-      const foundQuote = await getQuote();
-      setQuote(foundQuote);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
+    const handleQuote = async () => {
+      try {
+        const foundQuote = await getQuote();
+        setQuote(foundQuote);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     handleQuote();
   }, []);
 
